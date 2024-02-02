46853686173c4290bf01623e4efd9b004b8a6b4f
Lashmarr <156346074+Lashmarr@users.noreply.github.com>
2024-02-02 11:27:13 +0000

    file creation

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: tjhla
+ *
+ * Created on 02 February 2024, 11:16
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ tester.c
@@ -0,0 +1,15 @@
+/*
+ * File:   tester.c
+ * Author: tjhla
+ *
+ * Created on 02 February 2024, 11:15
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
